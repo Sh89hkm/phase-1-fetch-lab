b404c055d3755aae979b4ed14b7c7474e107c91e
shvan <shvanabdulhakeem2020@gmail.com>
2024-03-09 21:54:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+          .then((resp) => resp.json())
+          .then((json) => renderBooks(json));
 }
 
 function renderBooks(books) {
@@ -15,3 +17,15 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+/*
+// Adding a number before each book name
+function renderBooks(books) {
+  const main = document.querySelector('main');
+  books.forEach((book, index) => {                // Adding the number
+    const h2 = document.createElement('h2');
+    h2.innerHTML = `${index + 1}. ${book.name}`; // Adding the number
+    main.appendChild(h2);
+  });
+}
+*/
